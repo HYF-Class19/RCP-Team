350c2ec4d8a4ebf9e1fad3d5383aa33ba1bc2f12
bibhasingh <bibhaanil@gmail.com>
2023-03-24 14:45:19 +0100

    adding footer component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
-import Header from './components/Header';
-import NavBar from './components/NavBar';
-
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 export default function Home() {
   return (
     <>
       <Header />
       <NavBar />
+      <Footer />
     </>
   );
 }
