3f5fc6ba0f63bb595c20c82f3ebc4f185da873cd
John Edelbi <iceflaming@gmail.com>
2023-03-26 13:47:02 +0200

    clean page unnecessary files, imports

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
 'use client'
-import Header from "./components/Header";
-import NavBar from "./components/NavBar";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
+import { Header } from "./components/Header";
+import { NavBar } from "./components/NavBar";
 import { SlideShow } from './components/SlideShow';
-import Footer from "./components/Footer";
+import { Footer } from "./components/Footer";
 
 
 export default function Home() {
