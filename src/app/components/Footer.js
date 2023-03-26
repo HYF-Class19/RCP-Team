30450727ed20f4ae85722bc70c8b2e3b1d35c5e7
John Edelbi <iceflaming@gmail.com>
2023-03-26 13:34:04 +0200

    clean unnecessary files and imports to keep it on page.js

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,41 +1,33 @@
 "use client";
-import React from "react";
-import "primeicons/primeicons.css";
 import "./Footer.css";
-import "/node_modules/primeflex/primeflex.css";
-// import "primereact/resources/themes/vela-green/theme.css";
 import { Button } from "primereact/button";
 
-const Footer = () => {
+export const Footer = () => {
   return (
-    <>
-      <div className="footer">
-        <div className="flex gap-4 align-self-center buttons">
-          <a href="#">
-            <Button label="About Us" className="footerB" />
-          </a>
+    <div className="footer">
+      <div className="flex gap-4 align-self-center buttons">
+        <a href="#">
+          <Button label="About Us" className="footerB" />
+        </a>
 
-          <a href="#">
-            <Button label="Contact" className="footerB" />
-          </a>
-          <a href="#">
-            <Button label="Terms Of Use" className="footerB" />
-          </a>
-        </div>
-        <div className="align-content-end  flex gap-4 align-self-center icons">
-          <a href="https://fr-fr.facebook.com/">
-            <i className="pi pi-facebook iconStyle" />
-          </a>
-          <a href="https://www.instagram.com/">
-            <i className="pi pi-instagram iconStyle" />
-          </a>
-          <a href="https://twitter.com/?lang=fr">
-            <i className="pi pi-twitter iconStyle" />
-          </a>
-        </div>
+        <a href="#">
+          <Button label="Contact" className="footerB" />
+        </a>
+        <a href="#">
+          <Button label="Terms Of Use" className="footerB" />
+        </a>
       </div>
-    </>
+      <div className="align-content-end  flex gap-4 align-self-center icons">
+        <a href="https://fr-fr.facebook.com/">
+          <i className="pi pi-facebook iconStyle" />
+        </a>
+        <a href="https://www.instagram.com/">
+          <i className="pi pi-instagram iconStyle" />
+        </a>
+        <a href="https://twitter.com/?lang=fr">
+          <i className="pi pi-twitter iconStyle" />
+        </a>
+      </div>
+    </div>
   );
 };
-
-export default Footer;
