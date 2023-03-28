1bc5ac2077479cb9a8f32983d05f23ed33705eef
BrianMunene96 <107561877+BrianMunene96@users.noreply.github.com>
2023-03-29 01:38:22 +0200

    add pagination tot he recipe display

--- src/app/components/FilterComponent/ShowRecipes.js
+++ src/app/components/FilterComponent/ShowRecipes.js
@@ -1,29 +1,81 @@
+import React, { useState, useEffect } from 'react';
 import './ShowRecipes.css';
-import Image from 'next/image';
-import recipeImage from '../../../../public/assets/food2.jpg';
 import 'primeicons/primeicons.css';
+import { RecipeCard } from './RecipeCard';
+import { Paginate } from './Paginate';
 
 export const ShowRecipes = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [url, setUrl] = useState(
+    'https://api.spoonacular.com/recipes/complexSearch?cuisine=french,american,chinese,italian,african&number=56&apiKey=dd5c172973c04526bd48a30d4926cafb'
+  );
+  const [loading, setLoading] = useState(true);
+
+  // HANDLE PAGINATION
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(9);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const fetchRecipes = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const dataResults = data.results;
+    console.log(dataResults);
+    getRecipeInformation(dataResults);
+    setLoading(false);
+  };
+
+  const getRecipeInformation = async (response) => {
+    response.map(async (item) => {
+      const recipe = await fetch(
+        `https://api.spoonacular.com/recipes/${item.id}/information?apiKey=dd5c172973c04526bd48a30d4926cafb`
+      );
+
+      const recipeData = await recipe.json();
+      setRecipes((state) => {
+        state = [...state, recipeData];
+        console.log('STATE', state);
+        return state;
+      });
+    });
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+  }, [url]);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = recipes.slice(indexOfFirstPost, indexOfLastPost);
+
+  const previousPage = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const nextPage = () => {
+    if (currentPage !== Math.ceil(recipes.length / postsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
   return (
-    <div className="show-recipes">
-      <div className="single-recipe">
-        <Image
-          class="border-round-3xl"
-          className="recipe-image"
-          src={recipeImage}
-          alt="Image"
-          width={220}
-          height={156}
+    <div className="recipes-panel">
+      <div className="show-recipes">
+        <RecipeCard recipes={currentPosts} loading={loading} />
+      </div>
+      <div className="show-pages">
+        <Paginate
+          postsPerPage={postsPerPage}
+          totalPosts={recipes.length}
+          paginate={paginate}
+          nextPage={nextPage}
+          previousPage={previousPage}
         />
-        <div className="time-difficulty">
-          <h3>Easy</h3>
-          <h3>
-            45 <i className="pi pi-clock"></i>
-          </h3>
-        </div>
-        <div className="recipe-title">
-          <h3>Belgian Waffles with egss</h3>
-        </div>
       </div>
     </div>
   );
--- src/app/components/SlideShow.js
+++ src/app/components/SlideShow.js
@@ -5,45 +5,61 @@ import { classNames } from 'primereact/utils';
 import Image from 'next/image';
 import styles from './SlideShow.module.css';
 
-
 export const SlideShow = () => {
-    const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
-    const fetchRecipes = async () => {
-        const response = await fetch(`https://api.spoonacular.com/recipes/random?number=3&apiKey=yourkey`);
-        const data = await response.json();
-        setRecipes(data.recipes);
-    }
-
-    useEffect(() => {
-        fetchRecipes();
-    }, []);
-
-    const itemTemplate = (recipe) => {
-        return (
-            <div className="flex align-content-center">
-                <div className='flex-1 align-items-stretch'>
-                    <h2 className='mb-1'>{recipe.title}</h2>
-                    <Button label="Check Recipe" severity="secondary" icon="pi pi-search" />
-                </div>
-                <div className="flex-1 align-items-stretch">
-                    <Image className="w-8 shadow-2" src={recipe.image} alt={recipe.title} width={525} height={350} />
-                </div>
-            </div>
-
-        );
-    }
-
-    return (
-        <div className={styles.container}>
-            <div className={classNames('align-content-center', styles.slide)}>
-                {recipes?.length > 0 ? (
-                    <Carousel value={recipes} itemTemplate={itemTemplate} numVisible={1} numScroll={1} circular autoplayInterval={3000} />
-                ) : (
-                    <p>Loading...</p>
-                )}
-            </div>
-        </div>
+  const fetchRecipes = async () => {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/random?number=3&apiKey=yourkey`
     );
-}
+    const data = await response.json();
+    setRecipes(data.recipes);
+  };
 
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+  const itemTemplate = (recipe) => {
+    return (
+      <div className="flex align-content-center">
+        <div className="flex-1 align-items-stretch">
+          <h2 className="mb-1">{recipe.title}</h2>
+          <Button
+            label="Check Recipe"
+            severity="secondary"
+            icon="pi pi-search"
+          />
+        </div>
+        <div className="flex-1 align-items-stretch">
+          <Image
+            className="w-8 shadow-2"
+            src={recipe.image}
+            alt={recipe.title}
+            width={525}
+            height={350}
+          />
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={classNames('align-content-center', styles.slide)}>
+        {recipes?.length > 0 ? (
+          <Carousel
+            value={recipes}
+            itemTemplate={itemTemplate}
+            numVisible={1}
+            numScroll={1}
+            circular
+            autoplayInterval={3000}
+          />
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
+  );
+};
