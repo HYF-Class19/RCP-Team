e64312e2ed0bd7be44e53fbd9ff27ec17a4fbc59
bibhasingh <bibhaanil@gmail.com>
2023-03-29 21:56:05 +0200

    single recipe

--- src/app/components/SingleRecipe.js
+++ src/app/components/SingleRecipe.js
@@ -4,19 +4,28 @@ import "./SingleRecipe.css";
 import { Button } from "primereact/button";
 
 export const SingleRecipe = (props) => {
+  const [recipe, setRecipes] = useState([]);
   const fetchRecipe = async () => {
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/${props.dishId}/information?includeNutrition=true&apiKey=yourkey`
+      `https://api.spoonacular.com/recipes/${props.dishId}/information?includeNutrition=true&apiKey=${process.env.NEXT_PUBLIC_API_KEY}`
     );
     const data = await response.json();
-    console.log(data);
+    setRecipes(data);
   };
 
   useEffect(() => {
     fetchRecipe();
   }, []);
 
-  return <h1>SingleRecipe</h1>;
+  const displayRecipe = (recipe) => {
+    return <h1>{recipe.title}</h1>;
+  };
+
+  return (
+    <div>
+      {recipe.title ? <div>{displayRecipe(recipe)} </div> : <p>Loading...</p>}
+    </div>
+  );
 };
 
 export default SingleRecipe;
