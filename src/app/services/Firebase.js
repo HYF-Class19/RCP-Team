b5a5b25c76c4d30872220fa5cb5d3c852b7918a1
John Edelbi <iceflaming3@gmail.com>
2023-03-30 10:22:36 +0200

    fix firebase config

--- src/app/services/Firebase.js
+++ src/app/services/Firebase.js
@@ -10,10 +10,10 @@ import "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: NEXT_PUBLIC_FIREBASE_DB_URL,
-    projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 };
 
 // Initialize Firebase
