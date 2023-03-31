288258d198e65833c62f272347d37f69a2c5adb0
bibhasingh <107646355+bibhasingh@users.noreply.github.com>
2023-03-31 23:14:14 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,26 @@
-"use client";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
-import "primeflex/primeflex.css";
+'use client';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import './page.css';
 
-import { Header } from "./components/Header";
-import { NavBar } from "./components/NavBar";
-import { SlideShow } from "./components/SlideShow";
-import { SingleRecipe } from "./components/SingleRecipe";
-import { Footer } from "./components/Footer";
-import { Filter } from "./components/FilterComponent/Filter";
+import { Header } from './components/Header';
+import { NavBar } from './components/NavBar';
+import { SlideShow } from './components/SlideShow';
+import { Footer } from './components/Footer';
+import { Filter } from './components/FilterComponent/Filter';
+import { ShowRecipes } from './components/FilterComponent/ShowRecipes';
 
 export default function Home() {
   return (
     <>
       <Header />
       <NavBar />
-      {/* <SlideShow /> */}
-      <SingleRecipe dishId={324694} />
-      {/* <Filter /> */}
+      <SlideShow />
+      <div className="filter-showcase">
+        <Filter />
+        <ShowRecipes />
+      </div>
       <Footer />
     </>
   );
