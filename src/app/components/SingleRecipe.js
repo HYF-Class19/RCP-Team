2a7e1f1aa7b78421f347c0354f3e607da6985f6e
bibhasingh <bibhaanil@gmail.com>
2023-04-01 00:14:33 +0200

    single recipe

--- src/app/components/SingleRecipe.js
+++ src/app/components/SingleRecipe.js
@@ -12,7 +12,7 @@ export const SingleRecipe = (props) => {
 
   const fetchRecipe = async () => {
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/${props.dishId}/information?includeNutrition=true&apiKey=8638dc5c84554a86964bf04427dfdeee`
+      `https://api.spoonacular.com/recipes/${props.dishId}/information?includeNutrition=true&apiKey=yourApiKey`
     );
     const recipeInfo = await response.json();
     console.log(recipeInfo);
