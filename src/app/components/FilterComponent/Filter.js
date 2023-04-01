10e77aafafb76ed59720333a07fb084384bd206f
BrianMunene96 <107561877+BrianMunene96@users.noreply.github.com>
2023-04-01 11:30:41 +0200

    get selected data from filter components

--- src/app/components/FilterComponent/Filter.js
+++ src/app/components/FilterComponent/Filter.js
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { Dietary } from './Dietary';
 import { ExcludeIngredients } from './ExcludeIngredients';
 import './Filter.css';
@@ -8,6 +8,34 @@ import { MenuOrigin } from './MenuOrigin';
 import { Button } from 'primereact/button';
 
 export const Filter = () => {
+  const [includeIngredients, setIncludeIngredients] = useState();
+  const [excludedIngredients, setExcludeIngredients] = useState();
+  const [selectedDiets, setSelectedDiets] = useState();
+  const [selectedMenus, setSelectedMenus] = useState();
+
+  const getIngredients = (ingredients) => {
+    setIncludeIngredients(ingredients);
+  };
+
+  const getExcludedIngredients = (excludedIngredient) => {
+    setExcludeIngredients(excludedIngredient);
+  };
+
+  const getDiets = (selectedDiet) => {
+    setSelectedDiets(selectedDiet);
+  };
+
+  const getMenus = (selectedMenu) => {
+    setSelectedMenus(selectedMenu);
+  };
+
+  const handleClick = () => {
+    console.log('includeIngredients', includeIngredients);
+    console.log('excludeIngredients', excludedIngredients);
+    console.log('selectedDiets', selectedDiets);
+    console.log('selectedMenus', selectedMenus);
+  };
+
   return (
     <div className="filter">
       <div className="filter-titles">
@@ -15,12 +43,16 @@ export const Filter = () => {
         <h2>INGREDIENT</h2>
       </div>
       <div className="select-ingredients">
-        <IncludeIngredients />
-        <ExcludeIngredients />
-        <MenuOrigin />
-        <Dietary />
+        <IncludeIngredients callbackIngredients={getIngredients} />
+        <ExcludeIngredients callbackExcluded={getExcludedIngredients} />
+        <MenuOrigin callbackMenu={getMenus} />
+        <Dietary callbackDiets={getDiets} />
       </div>
-      <Button label="Search" className="btn-filter" />
+      <Button
+        onClick={() => handleClick()}
+        label="Search"
+        className="btn-filter"
+      />
     </div>
   );
 };
