cd9cec7b5080098094d3fb63eb08e06e46ba892b
John Edelbi <iceflaming3@gmail.com>
2023-04-07 10:37:09 +0200

    Modify the code to comply with Firebase 9

--- src/app/services/Firebase.js
+++ src/app/services/Firebase.js
@@ -1,27 +1,33 @@
 // Import the functions you need from the SDKs you need
-import { firebase } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import "firebase/auth";
-import "firebase/database";
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore'
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-export const initFirebase = () => {
-    if (!firebaseConfig.app.length) {
-        firebase.initializeApp(firebaseConfig);
-    }
-};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
 
 
-// const analytics = getAnalytics(app);
+	/* if (typeof window !== "undefined") {
+		if ("measurementId" in firebaseConfig) {
+			getAnalytics(app);
+		} 
+	}*/
