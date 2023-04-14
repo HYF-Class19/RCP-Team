5548d5a20549ce32cd077c6b00441c1aac6f24aa
bibhasingh <bibhaanil@gmail.com>
2023-04-15 00:38:43 +0200

    fix navbar issue

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,174 +1,325 @@
-'use client';
-import './NavBar.css';
-import Link from 'next/link';
-import React, { useState } from 'react';
+"use client";
+import "./NavBar.css";
+import Link from "next/link";
+import React, { useState } from "react";
 
+export const NavBar = () =>
+  // getDietsData,
+  // setSearchNewRecipes,
+  // callbackDiets,
+  {
+    // const [dietaryInfo, setDietaryInfo] = useState({ diets: [] });
+    // const [selectedDietary, setSelectedDietary] = useState();
 
+    // const dietaryChange = (e) => {
+    //   const { value, checked } = e.target;
 
-export const NavBar = ({ getDietsData, setSearchNewRecipes, callbackDiets }) => {
+    //   const { diets } = dietaryInfo;
 
-  const [dietaryInfo, setDietaryInfo] = useState({ diets: [] });
-  const [selectedDietary, setSelectedDietary] = useState();
+    //   if (checked) {
+    //     setDietaryInfo({
+    //       diets: [...diets, value],
+    //     });
+    //   } else {
+    //     setDietaryInfo({
+    //       diets: diets.filter((e) => e !== value),
+    //     });
+    //   }
+    // };
 
-  const dietaryChange = (e) => {
-    const { value, checked } = e.target;
+    // // callbackDiets(dietaryInfo);
 
-    const { diets } = dietaryInfo;
+    // const getDietary = (selectedDiet) => {
+    //   setSelectedDietary(selectedDiet);
+    // };
 
-    if (checked) {
-      setDietaryInfo({
-        diets: [...diets, value],
-      });
-    } else {
-      setDietaryInfo({
-        diets: diets.filter((e) => e !== value),
-      });
-    }
-  };
+    // const dietaryClick = () => {
+    //   setSearchNewRecipes(selectedDietary);
+    // };
 
-  // callbackDiets(dietaryInfo);
+    return (
+      <div className="navbar">
+        <input type="checkbox" id="toggle" />
+        <label htmlFor="toggle">
+          {" "}
+          Menu <i className="pi pi-bars"></i>
+        </label>
 
-  const getDietary = (selectedDiet) => {
-    setSelectedDietary(selectedDiet);
-  };
-
-  const dietaryClick = () => {
-    setSearchNewRecipes(
-      selectedDietary
-      
-     
-      
+        <ul className="parentList">
+          <li>
+            <Link
+              href={{
+                pathname: "../",
+              }}
+            >
+              <i
+                className="pi pi-home"
+                style={{ color: "white", fontSize: "1.5rem" }}
+              ></i>{" "}
+              Home
+            </Link>
+          </li>
+          <li>
+            <a href="#">
+              Menu Origin{" "}
+              <i className="pi pi-angle-down" style={{ color: "white" }}></i>
+            </a>
+            <ul>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "",
+                      menuOriginData: "indian",
+                    },
+                  }}
+                >
+                  Indian
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "",
+                      menuOriginData: "african",
+                    },
+                  }}
+                >
+                  African
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "",
+                      menuOriginData: "chinese",
+                    },
+                  }}
+                >
+                  Chinese
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "",
+                      menuOriginData: "american",
+                    },
+                  }}
+                >
+                  American
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "",
+                      menuOriginData: "european",
+                    },
+                  }}
+                >
+                  European
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "",
+                      menuOriginData: "french",
+                    },
+                  }}
+                >
+                  French
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "",
+                      menuOriginData: "italian",
+                    },
+                  }}
+                >
+                  Italian
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "",
+                      menuOriginData: "mediterranean",
+                    },
+                  }}
+                >
+                  Mediterranean
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "",
+                      menuOriginData: "mexican",
+                    },
+                  }}
+                >
+                  Mexican
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">
+              Dietary{" "}
+              <i className="pi pi-angle-down" style={{ color: "white" }}></i>
+            </a>
+            <ul>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "vegetarian",
+                      menuOriginData: "",
+                    },
+                  }}
+                >
+                  Vegetarian
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "vegan",
+                      menuOriginData: "",
+                    },
+                  }}
+                >
+                  Vegan
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "pescetarian",
+                      menuOriginData: "",
+                    },
+                  }}
+                >
+                  Pescetarian
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "primal",
+                      menuOriginData: "",
+                    },
+                  }}
+                >
+                  Primal
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={{
+                    pathname: "/components/FilterComponent",
+                    query: {
+                      ingredientsData: "",
+                      excludeIngredientsData: "",
+                      dietsData: "paleo",
+                      menuOriginData: "",
+                    },
+                  }}
+                >
+                  Paleo
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Top Recipes</a>
+          </li>
+          <li>
+            <Link
+              href={{
+                pathname: "/components/RandomRecipe",
+              }}
+            >
+              Random Recipe
+            </Link>
+          </li>
+        </ul>
+      </div>
     );
   };
-    
-
-
-  return (
-    <div className="navbar">
-      <input type="checkbox" id="toggle" />
-      <label htmlFor="toggle">
-        {' '}
-        Menu <i className="pi pi-bars"></i>
-      </label>
-
-      <ul className="parentList">
-        <li>
-          <Link
-            href={{
-              pathname: '../',
-            }}
-          >
-            <i
-              className="pi pi-home"
-              style={{ color: 'white', fontSize: '1.5rem' }}
-            ></i>{' '}
-            Home
-          </Link>
-        </li>
-        <li>
-          <a href="#">
-            Menu Origin{' '}
-            <i className="pi pi-angle-down" style={{ color: 'white' }}></i>
-          </a>
-          <ul>
-            <li>
-              <a href="#">Indian</a>
-            </li>
-            <li>
-              <a href="#">African</a>
-            </li>
-            <li>
-              <a href="#">Chinese</a>
-            </li>
-            <li>
-              <a href="#">American</a>
-            </li>
-            <li>
-              <a href="#">European</a>
-            </li>
-            <li>
-              <a href="#">French</a>
-            </li>
-            <li>
-              <a href="#">Italian</a>
-            </li>
-            <li>
-              <a href="#">Mediterranean</a>
-            </li>
-            <li>
-              <a href="#">Mexican</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#">
-            Dietary{' '}
-            <i className="pi pi-angle-down" style={{ color: 'white' }}></i>
-          </a>
-          <ul>
-            <li>
-              <a href="#"
-              type="checkbox"
-              id="vegetarian"
-              name="diets"
-              value="vegetarian"
-              onChange={dietaryChange}
-              onClick={() => dietaryClick('vegetarian')}>Vegetarian</a>
-            </li>
-            <li>
-              <a href="#"
-             onClick={() => {
-              handleClick("Vegan");
-            }}>Vegan</a>
-            </li>
-            <li>
-              <a href="#">Pescetarian</a>
-            </li>
-            <li>
-              <a href="#">Primal</a>
-            </li>
-            <li>
-              <a href="#">Paleo</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#">Top Recipes</a>
-        </li>
-        <li>
-          <Link
-            href={{
-              pathname: '/components/RandomRecipe',
-            }}
-          >
-            Random Recipe
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
-};
-
 
 // const handleClick = async (diet) => {
-  //   try {
-  //   const res = await fetch(
-  //   `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${diet}&diet=${diet}&number=10&limitLicense=false`,
-  //   {
-  //   method: 'GET',
-  //   headers: {
-  //   'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-  //   'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-  //   }
-  //   }
-  //   );
-  //   const data = await res.json();
-  //   setSearchNewRecipes(data.results);
-  //   } catch (error) {
-  //   console.error(error);
-  //   }
-  //   }
-    
+//   try {
+//   const res = await fetch(
+//   `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${diet}&diet=${diet}&number=10&limitLicense=false`,
+//   {
+//   method: 'GET',
+//   headers: {
+//   'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+//   'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+//   }
+//   }
+//   );
+//   const data = await res.json();
+//   setSearchNewRecipes(data.results);
+//   } catch (error) {
+//   console.error(error);
+//   }
+//   }
 
 // const [dietSearch, setDietSearch] = useState('')
 
