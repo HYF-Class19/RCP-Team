02b82a3c0bb972ddd16123f70661590eade4ce0f
John Edelbi <iceflaming3@gmail.com>
2023-04-18 12:10:32 +0200

    fix the api error

--- src/app/api/SpoonacularAPI.js
+++
@@ -1,8 +0,0 @@
-
-export const options = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key": process.env.SPOONACULAR_API_KEY,
-          "X-RapidAPI-Host": process.env.SPOONACULAR_API_HOST,
-        },
-      };
